8526c1f2c392c1c6704da5a0aec840ee8422a096
Jordan Comas <jordancomas1@yahoo.com>
2022-06-09 18:15:13 -0400

    finished lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,15 +1,51 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(x,y) {
+  if (x>y) {
+    return x
+  } else {
+    return y
+  }
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(arr) { if (arr.length ===0){
+  return null
+} 
+  let longestword = ''
+  
+  for (let i = 0; i < arr.length; i++) {
+
+    if(arr.length===0){
+      return null
+    }
+    if (arr[i].length > longestword.length) {
+      longestword = arr[i]
+    }
+  }
+  console.log(longestword)
+  return longestword;
+}
+
+findLongestWord(words)
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(num) {
+  if (num.length === 0) {
+    return 0
+  }
+  let sum = 0
+  for(let i = 0; i < num.length; i++) {
+  sum += num[i]
+  } 
+  console.log(sum)
+  return sum
+}
+
+sumNumbers(numbers)
 
 // Iteration #3.1 Bonus:
 function sum() {}
@@ -18,12 +54,34 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(num) {
+  if (num.length === 0) {
+    return null
+  }
+  let sum = 0
+  for(let i = 0; i < num.length; i++) {
+  sum += num[i]
+  } 
+  return sum/num.length 
+}
+
+averageNumbers(numbersAvg)
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() {}
+function averageWordLength(avg) {
+  if (avg.length===0){
+    return null
+  }
+  let elon = 0
+  for(let i=0; i < avg.length; i++){
+  elon += avg[i].length/avg.length
+  }
+  return elon
+}
+
+averageWordLength(wordsArr)
 
 // Bonus - Iteration #4.1
 function avg() {}
@@ -43,7 +101,20 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(arr){
+  if(arr.length === 0) {
+    return null
+  }
+  let newArr = []
+  for (let i=0; i < arr.length; i++){
+    if (newArr.indexOf(arr[i])===-1){
+      newArr.push(arr[i])
+    }
+  }
+  console.log(newArr)
+  return newArr
+}
+
 
 // Bonus - Iteration #5.1: Unique arrays using Set
 
@@ -52,7 +123,19 @@ function uniquifyArrayWithSet() {}
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(wordsFind, specificWord) {
+  if (wordsFind.length === 0) {
+    return null
+  }
+  for (let i=0; i < wordsFind.length; i++){
+    if ((wordsFind[i]) === specificWord) {
+      return true 
+    }    
+  }
+  return false
+}
+
+doesWordExist(wordsFind, `matter`)
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -68,8 +151,20 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+let word = `machine`
 
-function howManyTimes() {}
+function howManyTimes(wordsCount, word){
+
+ let count = 0 
+  
+for (let i=0; i < wordsCount.length; i++) {
+  if (word === wordsCount[i]) {
+    count++
+  }
+}
+  return count
+}
+howManyTimes(wordsCount, word)
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
